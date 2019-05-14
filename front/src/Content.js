a0ce19dcfdc20668bba0b12cb4268f6a886b3429
Zachary Lessner <LessnerZachary@gmail.com>
2019-05-14 17:55:56 -0400

    component did mount

--- front/src/Content.js
+++ front/src/Content.js
@@ -11,32 +11,28 @@ class Content extends Component {
 			capital: []
 		}
 
-		this.getCapital = this.getCapital.bind(this)
-
     }
     
 
-    getCapital() {
+    componentDidMount () {
 		fetch(`http://api.citybik.es/v2/networks`)
 		.then(res => res.json())
 		.then(json => {
 			console.log(json)
             this.setState({capital: json.networks})
-            console.log("before", this.state.capital)
+            //console.log("before", this.state.capital)
 		})
-	}
-
+	
+    }
 
  render() {
 
 
-    console.log("after", this.state.capital)
+    //console.log("after", this.state.capital)
     const capital = this.state.capital.length > 0 ? <p>{this.state.capital[0].name}</p> : null
-
     return (
-        <div  onClick={this.getCapital}>
-     
-
+        <div>
+    
         <h1>hi</h1>
             
           {capital}
@@ -45,23 +41,5 @@ class Content extends Component {
     );
 }
 }
-
-
-//     let riding = this.state.woof.map(item => {
-//         return (
-//           <div>
-//           <h2> <Link>  {item} </Link></h2>
-//           </div>
-//         )
-//       })
-
-//         return (
-//             <div>
-//                 <h1>hello</h1>
-//                 {riding}
-//             </div>
-//         );
-//     }
-// }
 
 export default Content;
