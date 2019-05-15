bf6dde3da950c7839410d56377c02c5175d3fa53
Zachary Lessner <LessnerZachary@gmail.com>
2019-05-15 11:26:52 -0400

    update

--- front/src/Search.js
+++ front/src/Search.js
@@ -4,65 +4,6 @@ import {Route, Link, Switch, Redirect} from 'react-router-dom'
 
 class Search extends Component {
 
-// 	constructor() {
-// 		super()
-
-// 		this.state = {
-//             capital: [],
-//             oink:''
-//         }
-
-//         this.searchCity = this.searchCity.bind(this);
-        
-//     }
-
-//   searchCity(evt) {
-//     this.setState({oink: evt.target.value})
-//   }
-    
-//     componentDidMount () {
-//         fetch(`http://api.citybik.es/v2/networks`)
-// 		.then(res => res.json())
-// 		.then(json => {
-// 			console.log(json.network)
-//             this.setState({capital: json.network})
-            
-// 		})
-// 	}
-
-
-// 	render() {
-// console.log(this.state.capital)
-//         let listOfBikes = this.state.capital.map(outcome => {
-//             return (
-//                 <div className="bikeSearch">
-//               <h1> <Link to={"/search"}>{outcome.location.latitude} </Link></h1>
-//               <p>{outcome.name}</p>
-//               </div>
-//             )
-//             })
-    
-//           return (
-//             <div>
-    
-//               <div className="searchBox">
-//           <input 
-//             type="text"
-//              placeholder="Search City"
-//              onChange={this.searchCity}
-//              />
-//            <button type="submit">Search</button>
-//         </div>
-//               <div className='container'> {listOfBikes}</div>
-    
-//             </div>
-            
-            
-//           )
-    
-//     }
-//     }
-
 constructor() {
     super()
 
